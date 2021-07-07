0f82582909c5c34c465a5d68f9af754509312207
rockit-ba <1121153008@qq.com>
2021-07-07 18:42:14 +0800

    'update'

---
+++ java-thread/src/main/java/threadlocal/ThreadLocalDemo.java
@@ -0,0 +1,34 @@
+package threadlocal;
+
+/**
+ * @author lucky
+ */
+public class ThreadLocalDemo {
+    private static final ThreadLocal<String> THREAD_LOCAL = ThreadLocal.withInitial(() -> "hello");
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            THREAD_LOCAL.set(Thread.currentThread().getName() + THREAD_LOCAL.get());
+            System.out.println(THREAD_LOCAL.get());
+        });
+
+        Thread thread2 = new Thread(() -> {
+            THREAD_LOCAL.set(Thread.currentThread().getName() + THREAD_LOCAL.get());
+            System.out.println(THREAD_LOCAL.get());
+        });
+
+
+        thread.start();
+        thread2.start();
+        System.out.println(THREAD_LOCAL.get());
+
+    }
+
+
+
+
+
+
+
+
+}
