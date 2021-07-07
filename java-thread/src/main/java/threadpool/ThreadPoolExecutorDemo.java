88d75ad2e78df20e678f14aecbcbf668aad8f598
rockit-ba <1121153008@qq.com>
2021-07-07 15:40:29 +0800

    'update'

---
+++ java-thread/src/main/java/threadpool/ThreadPoolExecutorDemo.java
@@ -0,0 +1,23 @@
+package threadpool;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author lucky
+ */
+public class ThreadPoolExecutorDemo {
+
+    public static void main(String[] args) {
+        /**
+         *  corePoolSize        :核心线程数，即使空闲也不会被回收
+         *  maximumPoolSize     ：可创建的最大线程数
+         *  keepAliveTime  unit     ：线程最大空闲时长，时长单位
+         *  workQueue            ：任务的排队队列，只能是阻塞队列（BlockingQueue<Runnable>）
+         *  threadFactory       ：新线程创建方式
+         *  handler             ：拒绝策略
+         *
+        **/
+
+    }
+
+}
